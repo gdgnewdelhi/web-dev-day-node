27d61481594ee7514c5f2fbd395551fb11e53505
arpansac <arpansac@gmail.com>
2019-02-03 02:16:59 +0530

    use cors for Angular

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const cors = require('cors');
+
+app.use(cors());
 
 // [TODO 4] setup the connection with db by requiring it from config/mongoose.js
 
